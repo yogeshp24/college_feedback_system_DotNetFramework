899622df8ac55e7bcd27e666c1098d81e8e186d1
Yogesh Patil <patilyoge625@gmail.com>
2025-04-18 12:59:53 +0530

    Add files via upload

---
+++ feedbackController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security;
+using collegefeedbacksystem24.Models;
+
+namespace xyzproject.Controllers
+{
+    public class feedbackController : Controller
+    {
+
+        CollegeFeedbackSystemEntities db = new CollegeFeedbackSystemEntities();
+
+
+        [Authorize]
+        public ActionResult feedback()
+        {
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+
+        public ActionResult feedback(feedback f)
+        {
+            if (ModelState.IsValid)
+            {
+
+                db.feedbacks.Add(f);
+                int i = db.SaveChanges();
+
+                if (i > 0)
+                {
+
+                    return RedirectToAction("home", "home");
+                }
+
+            }
+
+
+
+            return View();
+        }
+
+
+        [Authorize]
+        public ActionResult feedbacks()
+        {
+            var s = db.feedbacks.ToList();
+
+            return View(s);
+        }
+
+
+        [Authorize]
+        public ActionResult details(int id)
+        {
+            var s = db.feedbacks.FirstOrDefault(m => m.StudentId == id);
+
+            return View(s);
+        }
+
+        [Authorize]
+        public ActionResult delete(int id)
+        {
+            var s = db.feedbacks.FirstOrDefault(m => m.StudentId == id);
+
+            return View(s);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            var feedback = db.feedbacks.FirstOrDefault(f => f.StudentId == id);
+            if (feedback != null)
+            {
+                db.feedbacks.Remove(feedback);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Feedbacks");
+        }
+
+
+
+    }
+}
---
+++ homeController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using collegefeedbacksystem24.Models;
+
+namespace xyzproject.Controllers
+{
+    public class homeController : Controller
+    {
+
+        CollegeFeedbackSystemEntities db = new CollegeFeedbackSystemEntities();
+
+
+        [Authorize]
+        public ActionResult home()
+        {
+            return View();
+        }
+
+
+        [Authorize]
+        public ActionResult aboutus()
+        {
+
+            return View();
+        }
+
+
+        [Authorize]
+        public ActionResult contactus()
+        {
+
+            return View();
+        }
+
+
+        [Authorize]
+        public ActionResult facilites()
+        {
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult faculty()
+        {
+
+            return View();
+        }
+
+
+        [Authorize]
+        public ActionResult Courses()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult register(userregistration reg)
+        {
+            if (ModelState.IsValid)
+            {
+
+                db.userregistrations.Add(new userregistration
+                {
+                    uid = reg.uid,
+                    uname = reg.uname,
+                    uemail = reg.uemail,
+                    upass = reg.upass,
+                    crole = reg.crole
+                });
+                db.SaveChanges();
+
+                return RedirectToAction("Login");
+            }
+
+            return View(reg);
+
+        }
+
+
+
+
+        public ActionResult login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult login(userregistration reg)
+        {
+
+            var s = db.userregistrations.FirstOrDefault(m => m.uname == reg.uname && m.upass == reg.upass);
+
+            if (s != null)
+            {
+                FormsAuthentication.SetAuthCookie(reg.uname, true);
+                return RedirectToAction("home");
+            }
+
+            else
+            {
+                return RedirectToAction("register");
+            }
+
+
+
+        }
+
+
+        public ActionResult logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("login");
+        }
+
+
+
+
+
+
+
+
+    }
+}
